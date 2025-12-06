1e9d3d7bfdb3cecf759b3446738337e9909124b9
ChimitG <chimitodin@gmail.com>
2025-12-06 12:28:25 +0700

    Update Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,6 +1,6 @@
 import gensim
 word2vec = gensim.models.KeyedVectors.load_word2vec_format("cbow.txt", binary=False)
-pos=["зарисовка_NOUN", "арка_NOUN"]
+pos=["портик_NOUN", "аркада_NOUN","зарисовка_NOUN"]
 neg=["настен_NOUN"]
 dist = word2vec.most_similar(positive=pos, negative=neg)
 for i in dist:
